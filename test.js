4f90c27b8885d2931cbe56ad138eab5f587546e1
Shivam Bhardwaj <60943738+shivambhardwaj123@users.noreply.github.com>
2023-08-14 14:25:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+// import emailjs from "emailjs-com";
+// const Mailer = () => {
+//     function sendEmail(e) {
+//       e.preventDefault();
+
+//       emailjs.submit(
+//           "service_mzw2cc7",
+//           "template_vkl8b7u",
+//       e.target,
+//       "user_NfKBkPOoWhJe8NGGerkuV"
+//       ).then(res=>{
+//           console.log(res);
+//       }).catch(err=> console.log(err));
+
+//     };
+// };
